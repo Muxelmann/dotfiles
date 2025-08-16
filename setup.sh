4ea45c1e95337f846c4a75ec6e5a81146ba86a89
Max Zangs <maximilian.zangs@gmail.com>
2025-08-16 10:51:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,24 +5,6 @@
 # by Max Zangs
 #
 
-# Copy dotfiles
-cp .zprofile $HOME
-cp .aliases $HOME
-
-# Load profile for coloring
-. ~/.zprofile
-
-# Test if everything should be installed by default
-if [ -n "$1" -a "$1" = "-all" ]; then
-    print -P "%F{red}Everything will be installed, i.e. asking Y/N is skipped!%f"
-    install="everything"
-elif [ -n "$1" -a "$1" = "-skip" ]; then
-    print -P "%F{red}Everything non-essential will be skipped!%f"
-    install="essential only"
-else
-    install=""
-fi
-
 # Install Homebrew or update it
 if [ ! -d /opt/homebrew ]; then
     print -P "%F{green}Installing Hombrew%f"
@@ -42,15 +24,10 @@ end
 # Install Rosetta
 sudo softwareupdate --install-rosetta
 
-# Also install MacOS Applications included in the tap: homebrew/cask-drivers
-brew tap homebrew/cask-drivers
-
 # Install essential formulae
 
 brew install \
-    java \
     mas \
-    pyenv \
     sdl2 \
     sdl2_image \
     sdl2_mixer \
@@ -60,17 +37,18 @@ brew install \
 
 brew install --cask \
     blender \
+    citrix-workspace \
     devonthink \
     docker \
     fujitsu-scansnap-home \
     macdown \
+    mas \
     microsoft-powerpoint \
     microsoft-word \
     microsoft-excel \
     microsoft-teams \
     minecraft \
     moneymoney \
-    parallels \
     sensei \
     sonos \
     spotify \
@@ -79,24 +57,10 @@ brew install --cask \
 
 # Install apps from AppStore
 
-if [ -z $install ]; then
-    print -P "%F{green}Install further (non \"essential\") applications from AppStore? [y/n]%f "
-    read yn
-elif [ $install = "everything" ]; then
-    yn="y"
-elif [ $install = "essential only" ]; then  
-    yn="n"
-fi
-
-if [ $yn = "y" -o $yn = "Y" ]; then
-    brew install mas
-    mas install 409203825 # Numbers
-    mas install 409201541 # Pages
-    mas install 409183694 # Keynote
-    mas install 497799835 # Xcode
-    mas install 425264550 # Blackmagic Disk Speed Test
-    mas install 441258766 # Magnet
-fi
+mas install 409203825 # Numbers
+mas install 409201541 # Pages
+mas install 409183694 # Keynote
+mas install 497799835 # Xcode
 
 # Configure Visual Studio Code
 
